efa85ba92db9b653610474dd320fc5d2342b6422
arwinEst <arestaura09@gmail.com>
2026-04-01 05:15:30 -0400

    Updated to show lanes, playfield, and get inputs

--- Screens/GamePlayScreen.cs
+++ Screens/GamePlayScreen.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Comp_296_project_ARMA.Objects;
 
 namespace Comp_296_project_ARMA.Screens
 {
@@ -14,26 +15,51 @@ namespace Comp_296_project_ARMA.Screens
         private SpriteFont _font;
         private SpriteBatch _spriteBatch;
         private ScreenManager _screenManager;
-
+        private Texture2D _background;
+        private GraphicsDevice _graphicsDevice;
+        private Playfield _playfield;
         private KeyboardState _currentState;
         private KeyboardState _previousState;
 
-        public GamePlayScreen(SpriteFont font, SpriteBatch spriteBatch, ScreenManager screenManager)
+        private bool [] _lanePressed = new bool[4];
+
+        public GamePlayScreen(SpriteFont font, Texture2D background, SpriteBatch spriteBatch, ScreenManager screenManager,
+            GraphicsDevice graphicsDevice)
         {
             _font = font;
             _spriteBatch = spriteBatch;
             _screenManager = screenManager;
+            _background = background;
+            _graphicsDevice = graphicsDevice;
+            _playfield = new Playfield(graphicsDevice);
         }
 
-        public override void Update(GameTime gameTime)
+        public void Load()
         {
-            
+
         }
 
-        public override void Draw(SpriteBatch spriteBatch)
+        public void Update(GameTime gameTime)
         {
-            spriteBatch.DrawString(_font, "Gameplay",
-                new Vector2(100, 400), Color.White);
+            _previousState = _currentState;
+            _currentState = Keyboard.GetState();
+
+            // Check which lanes are pressed
+            _lanePressed[0] = _currentState.IsKeyDown(Keys.D);
+            _lanePressed[1] = _currentState.IsKeyDown(Keys.F);
+            _lanePressed[2] = _currentState.IsKeyDown(Keys.J);
+            _lanePressed[3] = _currentState.IsKeyDown(Keys.K);
+
+            _playfield.Update(_lanePressed);
+
+            // Pause Menu
+          
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            _playfield.Draw(spriteBatch);
         }
 
     }
