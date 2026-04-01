8b67fdcad75d6b21afb125f1dcc9b58da8730816
arwinEst <arestaura09@gmail.com>
2026-04-01 05:16:51 -0400

    Connects to database. Opens and closes database

---
+++ Data/DatabaseManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+namespace Comp_296_project_ARMA.Data
+{
+    public class DatabaseManager
+    {
+        public void Initialize() {
+            //Path of database
+            var connection = new SqliteConnection("Data Source=arma.db");
+
+            connection.Open();
+
+            connection.Close();
+
+        }
+
+    }
+}
+
