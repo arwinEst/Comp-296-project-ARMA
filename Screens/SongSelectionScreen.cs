baaaa22794d8e4aa40a11a58c2ef51b1095622e5
arwinEst <arestaura09@gmail.com>
2026-04-22 11:48:21 -0400

    updated
    updated to load chart when going into Gameplay Screen. Removed list place holder. Gets charts from database and displays said charts so the user can choose which chart to play.

--- Screens/SongSelectionScreen.cs
+++ Screens/SongSelectionScreen.cs
@@ -6,34 +6,85 @@ using Microsoft.Xna.Framework.Input;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Comp_296_project_ARMA.Data;
+using Comp_296_project_ARMA.Objects;
 
 namespace Comp_296_project_ARMA.Screens
 {
     public class SongSelectionScreen : GameScreen
     {
         private SpriteFont _font;
+        private Texture2D _background;
         private SpriteBatch _spriteBatch;
+        private ScreenManager _screenManager;
+        private GraphicsDevice _graphicsDevice;
 
-        public SongSelectionScreen(SpriteFont font, SpriteBatch spriteBatch)
+        private List<ChartEntry> GetAllCharts()
+        {
+            var charts = new List<ChartEntry>();
+
+            using (var connection = new SqliteConnection("Data Source = arma.db"))
+            {
+                connection.Open();
+                string query = "SELECT * From Charts ORDER BY Title";
+
+                using (var command = new SqliteCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            charts.Add(new ChartEntry
+                            {
+                                Id = reader.GetInt32(0),
+                                Title = reader.GetString(1),
+                                Artist = reader.GetString(2),
+                                AudioFile = reader.GetString(3),
+                                BPM = reader.GetDouble(4),
+                                Offset = reader.GetDouble(5),
+                                FilePath = reader.GetString(6)
+                            });
+                        }
+                    }
+                }
+                return charts;
+            }
+        }
+
+        private List<ChartEntry> _charts;
+        //private DatabaseManager _databaseManager;
+
+        public SongSelectionScreen(SpriteFont font, Texture2D background, 
+            SpriteBatch spriteBatch, ScreenManager screenManager, 
+            GraphicsDevice graphicsDevice)
         {
             _font = font;
+            _background = background;
             _spriteBatch = spriteBatch;
+            _screenManager = screenManager;
+            _graphicsDevice = graphicsDevice;
 
+            //Load charts from database
+            _charts = GetAllCharts();
         }
-        //Placeholder songs
-        private List<String> _songs = new List<string>
-        {
-            "Song 1",
-            "Song 2",
-            "Song 3"
 
-        };
         private int _selectedIndex = 0;
 
         private KeyboardState _currentState;
         private KeyboardState _previousState;
-        public override void Update(GameTime gameTime)
+
+        private bool _isFirstFrame = true;
+
+        public void Update(GameTime gameTime)
         {
+            if (_isFirstFrame)
+            {
+                _isFirstFrame = false;
+                _previousState = Keyboard.GetState();
+                _currentState = Keyboard.GetState();
+                return;
+            }
             // Handle song selection input and logic here
             _previousState = _currentState;
             _currentState = Keyboard.GetState();
@@ -45,9 +96,9 @@ namespace Comp_296_project_ARMA.Screens
             }
             if (_currentState.IsKeyDown(Keys.Down) && _previousState.IsKeyUp(Keys.Down))
             {
-                _selectedIndex = Math.Min(_songs.Count - 1, _selectedIndex + 1);
+                _selectedIndex = Math.Min(_charts.Count - 1, _selectedIndex + 1);
             }
-            // Select option
+            // Select Song
             if (_currentState.IsKeyDown(Keys.Enter) && _previousState.IsKeyUp(Keys.Enter))
             {
                 SelectSong();
@@ -59,25 +110,32 @@ namespace Comp_296_project_ARMA.Screens
         }
         private void SelectSong()
         {
-            // Logic to start the game with the selected song
-            // For example, you could switch to the gameplay screen and pass the selected song
+            ChartEntry selected = _charts[_selectedIndex];
+            Chart chart = ChartLoader.LoadChart(selected.FilePath);
+            _screenManager.SetScreen(new GamePlayScreen(_font, _background, _spriteBatch,
+                _screenManager, _graphicsDevice));
         }
 
         private void GoBack()
         {
             // Logic to return to the main menu
             // For example, you could switch back to the MainMenuScreen
+            _screenManager.SetScreen(new MainMenuScreen(_font, _background, _spriteBatch, _screenManager, _graphicsDevice));
+
         }
 
-        public override void Draw(SpriteBatch spriteBatch)
+        public void Draw(SpriteBatch spriteBatch)
         {
             // Draw the song selection UI here
+            spriteBatch.Draw(_background, Vector2.Zero, Color.White);
+
             spriteBatch.DrawString(_font, "Song Select:", new Vector2(100, 100), Color.White);
 
-            for (int i = 0; i < _songs.Count; i++)
+            for (int i = 0; i < _charts.Count; i++)
             {
                 Color color = (i == _selectedIndex) ? Color.Yellow : Color.White;
-                spriteBatch.DrawString(_font, _songs[i], new Vector2(300, 150 + i * 50), color);
+                string display = $"{_charts[i].Title} - {_charts[i].Artist}";
+                spriteBatch.DrawString(_font, display, new Vector2(100, 150 + i *50), color);
             }
             spriteBatch.DrawString(_font, "Press Enter to select, Esc to go back",
                 new Vector2(100, 400), Color.White);
