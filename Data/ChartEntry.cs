c582d0846f112990a8e41c1cc940a72b8ec2c2c0
arwinEst <arestaura09@gmail.com>
2026-04-22 11:31:49 -0400

    Gets the chart data

---
+++ Data/ChartEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Comp_296_project_ARMA.Data
+{
+    public class ChartEntry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string AudioFile { get; set; }
+        public double BPM { get; set; }
+        public double Offset { get; set; }
+        public string FilePath { get; set; }
+    }
+}
