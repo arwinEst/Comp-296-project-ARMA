052700baf9c8ff0ff26777cd6d136be34d6fd0da
arwinEst <arestaura09@gmail.com>
2026-04-22 11:50:00 -0400

    updated

--- obj/Debug/net9.0/Comp-296 project ARMA.AssemblyInfo.cs
+++ obj/Debug/net9.0/Comp-296 project ARMA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Comp-296 project ARMA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa029a0808a2ae21ea56f3cfb15e010837d3049")]
 [assembly: System.Reflection.AssemblyProductAttribute("Comp-296 project ARMA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Comp-296 project ARMA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
