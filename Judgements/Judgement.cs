6d4d7229102a6b3aa91fe22f5c54fdcad1ddeb89
arwinEst <arestaura09@gmail.com>
2026-04-22 12:17:01 -0400

    Creates judgement objects

---
+++ Judgements/Judgement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Comp_296_project_ARMA.Judgements
+{
+    public class Judgement
+    {
+       Marvelous,  
+       Perfect,
+       Great,
+       Good,
+       Bad,
+       Miss
+    }
+}
