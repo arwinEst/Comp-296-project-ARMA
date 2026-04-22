578fed88a51ff2bd38c1d0b81079960acbac6714
arwinEst <arestaura09@gmail.com>
2026-04-22 11:39:23 -0400

    deserializes the JSON file

---
+++ Objects/ChartLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Comp_296_project_ARMA.Objects
+{
+    public class ChartLoader
+    {
+        public static Chart LoadChart(string filePath)
+        {
+            // Implement file reading and parsing logic here
+            // For example, you could read a JSON or XML file and populate the Chart object
+            // This is just a placeholder implementation
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<Chart>(json);
+        }
+    }
+}
