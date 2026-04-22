d565f737ed0221914195efcd4cbd2f946cb1daa1
arwinEst <arestaura09@gmail.com>
2026-04-22 11:39:03 -0400

    initializes data of a chart

---
+++ Objects/Chart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Comp_296_project_ARMA.Objects;
+
+namespace Comp_296_project_ARMA.Objects
+{
+    public class Chart
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string AudioFile { get; set; }
+        public double BPM { get; set; }
+        public double Offset { get; set; }
+        public List<NoteObject> Notes { get; set; } = new List<NoteObject>();
+    }
+
+}
