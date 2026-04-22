03e7e3157b1a4e9c7ec6bc4f57d775ef1c0a5cfa
arwinEst <arestaura09@gmail.com>
2026-04-22 11:41:47 -0400

    initializes the objects for a note

---
+++ Objects/NoteObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Comp_296_project_ARMA.Objects
+{
+    public class NoteObject
+    {
+        public int Lane { get; set; }
+        public double HitTime { get; set; } // Time in milliseconds when the note should be hit
+        public int HoldEndTime { get; set; }
+        public bool IsHold { get; set; } // 0: Tap, 1: Hold, 2: Slide
+        public double Duration { get; set; } // Only for Hold and Slide notes
+    }
+
+}
+
+
