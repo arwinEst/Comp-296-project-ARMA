2a0e00dd14eec2fc062922dffdb972531491e3ea
arwinEst <arestaura09@gmail.com>
2026-04-22 11:42:53 -0400

    Objects for the playfield

---
+++ Objects/PlayFieldConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Comp_296_project_ARMA.Objects
+{
+    public static class PlayFieldConstants
+    {
+        public const int LaneCount = 4;
+        public const int LaneWidth = 120;
+        public const int ReceptorY = 900;
+        public const int NoteHeight = 30;
+        public static readonly float StartX = (1920 - (LaneWidth * LaneCount)) / 2f;
+    }
+}
