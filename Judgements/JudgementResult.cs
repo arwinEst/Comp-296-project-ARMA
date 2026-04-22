a2eed3b4c97d40cbe0d6224a12b2d1748a8b6873
arwinEst <arestaura09@gmail.com>
2026-04-22 12:17:20 -0400

    Gets the result of each judgement

---
+++ Judgements/JudgementResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Comp_296_project_ARMA.Judgements
+{
+    public class JudgementResult
+    {
+        public Judgement Judgement { get; set; }
+        public double HitDifference { get; set; } // How early or late the hit was in ms
+        public int Lane { get; set;  }
+    }
+}
+       
