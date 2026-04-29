ea6b6f0392f10581db6a05dd19492f82544317a0
arwinEst <arestaura09@gmail.com>
2026-04-29 00:26:03 -0400

    create scores table and saves scores

--- Data/DatabaseManager.cs
+++ Data/DatabaseManager.cs
@@ -1,6 +1,7 @@
 ﻿using Comp_296_project_ARMA.Objects;
 using Microsoft.Data.Sqlite;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace Comp_296_project_ARMA.Data
 {
@@ -28,6 +29,24 @@ namespace Comp_296_project_ARMA.Data
             using (var command = new SqliteCommand(createChartsTable, connection))
                 command.ExecuteNonQuery();
 
+            string createScoresTable = @"
+                CREATE TABLE IF NOT EXISTS Scores (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    songName TEXT NOT NULL,
+                    score INTEGER NOT NULL,
+                    combo INTEGER NOT NULL,
+                    accuracy REAL NOT NULL,
+                    marvelousCount INTEGER NOT NULL,
+                    perfectCount INTEGER NOT NULL,
+                    greatCount INTEGER NOT NULL,
+                    goodCount INTEGER NOT NULL,
+                    badCount INTEGER NOT NULL,
+                    missCount INTEGER NOT NULL,
+                    getGrade TEXT
+                )";
+            using (var command = new SqliteCommand(createScoresTable, connection))
+                command.ExecuteNonQuery();
+
             connection.Close();
 
         }
@@ -35,7 +54,7 @@ namespace Comp_296_project_ARMA.Data
         public void RegisterChart(Chart chart, string filePath)
         {
 
-            var connection = new SqliteConnection("Data Source=arma.db");
+            var connection = new SqliteConnection("Data Source = arma.db");
             
             connection.Open();
 
@@ -64,6 +83,34 @@ namespace Comp_296_project_ARMA.Data
             }
             connection.Close();
 
+        }
+
+        public void SaveScore(string songName, int score, int combo, double accuracy, int marvelousCount,
+            int perfectCount, int greatCount, int goodCount, int badCount, int missCount, string getGrade)
+        {
+            var connection = new SqliteConnection("Data Source = arma.db");
+
+            connection.Open();
+
+            string insertQuery = @"
+                    INSERT INTO Scores (songName, score, combo, accuracy, marvelousCount, perfectCount, greatCount, goodCount, badCount, missCount, getGrade)
+                    VALUES (@songName, @score, @combo, @accuracy, @marvelousCount, @perfectCount, @greatCount, @goodCount, @badCount, @missCount, @getGrade)";
+
+            using (var command = new SqliteCommand(insertQuery, connection))
+            {
+                command.Parameters.AddWithValue("@songName", songName);
+                command.Parameters.AddWithValue("@score", score);
+                command.Parameters.AddWithValue("@combo", combo);
+                command.Parameters.AddWithValue("@accuracy", accuracy);
+                command.Parameters.AddWithValue("@marvelousCount", marvelousCount);
+                command.Parameters.AddWithValue("@perfectCount", perfectCount);
+                command.Parameters.AddWithValue("@greatCount", greatCount);
+                command.Parameters.AddWithValue("@goodCount", goodCount);
+                command.Parameters.AddWithValue("@badCount", badCount);
+                command.Parameters.AddWithValue("@missCount", missCount);
+                command.Parameters.AddWithValue("@getGrade", getGrade);
+                command.ExecuteNonQuery();
+            }
         }
     }
 
