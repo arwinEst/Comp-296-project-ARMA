065d7bd0e9cd35ad5e834a2169d5dbf388d3d489
arwinEst <arestaura09@gmail.com>
2026-05-05 14:08:45 -0400

    constants changed to fit 2560x1440p

--- Objects/PlayFieldConstants.cs
+++ Objects/PlayFieldConstants.cs
@@ -6,8 +6,8 @@ namespace Comp_296_project_ARMA.Objects
     {
         public const int LaneCount = 4;
         public const int LaneWidth = 120;
-        public const int ReceptorY = 900;
+        public const int ReceptorY = 1260;
         public const int NoteHeight = 30;
-        public static readonly float StartX = (1920 - (LaneWidth * LaneCount)) / 2f;
+        public static readonly float StartX = (2560 - (LaneWidth * LaneCount)) / 2f;
     }
 }
