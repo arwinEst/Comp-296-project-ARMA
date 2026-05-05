f1fce56548d7588ec1110acca68055a062c73792
arwinEst <arestaura09@gmail.com>
2026-05-05 14:09:45 -0400

    updated

--- Game1.cs
+++ Game1.cs
@@ -11,8 +11,6 @@ namespace Comp_296_project_ARMA
 {
     public class Game1 : Game
     {
-
-        private InputManager inputManager;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private ScreenManager _screenManager;
@@ -41,8 +39,8 @@ namespace Comp_296_project_ARMA
 
         public Game1()
         {
-            _screenManager = new();
             _graphics = new GraphicsDeviceManager(this);
+            _screenManager = new();
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -58,28 +56,11 @@ namespace Comp_296_project_ARMA
             _graphics.IsFullScreen = true;
             _graphics.ApplyChanges();
             base.Initialize();
-            inputManager = new InputManager();
         }
 
         protected override void Update(GameTime gameTime)
         {
             _screenManager.Update(gameTime);
-            inputManager.Update();
-
-            if (inputManager.IsActionPressed(GameAction.Pause))
-            {
-                // Handle pause action (e.g., toggle pause menu)
-            }
-
-            if (inputManager.IsActionPressed(GameAction.HitLane1))
-            {
-                // Check if a note is in range for Lane 1 and register a hit
-            }
-
-
-            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                //Exit();
-
             // TODO: Add your update logic here
             
 
@@ -118,49 +99,4 @@ namespace Comp_296_project_ARMA
             _currentScreen?.Draw(spriteBatch);
         }
     }
-
-
-    // Enum to represent game actions
-    public enum GameAction
-    {
-        HitLane1,
-        HitLane2,
-        HitLane3,
-        HitLane4,
-        Pause
-    }
-
-    public class InputManager
-    {
-        private KeyboardState currentKeyboardState;
-        private KeyboardState previousKeyboardState;
-        public void Update()
-        {
-            previousKeyboardState = currentKeyboardState;
-            currentKeyboardState = Keyboard.GetState();
-        }
-        public bool IsActionPressed(GameAction action)
-        {
-            switch (action)
-            {
-                case GameAction.HitLane1:
-                    return IsKeyPressed(Keys.D);
-                case GameAction.HitLane2:
-                    return IsKeyPressed(Keys.F);
-                case GameAction.HitLane3:
-                    return IsKeyPressed(Keys.J);
-                case GameAction.HitLane4:
-                    return IsKeyPressed(Keys.K);
-                case GameAction.Pause:
-                    return IsKeyPressed(Keys.Escape);
-                default:
-                    return false;
-            }
-        }
-        private bool IsKeyPressed(Keys key)
-        {
-            return currentKeyboardState.IsKeyDown(key) && !previousKeyboardState.IsKeyDown(key);
-        }
-    }
-
  }
