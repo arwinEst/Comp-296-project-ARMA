5496cb1480de1e645faa1202ffaf18f100c134f3
arwinEst <arestaura09@gmail.com>
2026-05-04 20:56:51 -0400

    Render Hold Notes

--- Objects/NoteRenderer.cs
+++ Objects/NoteRenderer.cs
@@ -14,15 +14,35 @@ namespace Comp_296_project_ARMA.Objects
 
         public NoteRenderer(GraphicsDevice graphicsDevice, List<NoteObject> notes)
         {
-            _noteTexture = new Texture2D(graphicsDevice, 1, 1);
-            _noteTexture.SetData(new[] { Color.White }); // Placeholder white texture
-
             _notes = notes;
-            
+            _noteTexture = new Texture2D(graphicsDevice, 1, 1);
+            _noteTexture.SetData(new[] { Color.White }); // Placeholder white texture 
         }
 
-        public void Draw(SpriteBatch spriteBatch, double _currentTime)
+        public void Draw(SpriteBatch spriteBatch, double _currentTime, List<NoteObject> activeHoldNotes)
         {
+
+            // Draw hold bodies
+            foreach (var note in activeHoldNotes)
+            {
+                double timeDiff = note.HitTime - _currentTime;
+
+                float _noteYPosition = PlayFieldConstants.ReceptorY;
+
+                float _noteXPosition = PlayFieldConstants.StartX + note.Lane * PlayFieldConstants.LaneWidth;
+
+                Color _noteColor = note.Lane switch
+                {
+                    0 => Color.Red,
+                    1 => Color.CornflowerBlue,
+                    2 => Color.CornflowerBlue,
+                    3 => Color.Red,
+                    _ => Color.White
+                };
+
+                DrawHoldNote(spriteBatch, note, _noteXPosition, _noteYPosition, _noteColor, _currentTime);
+            }
+
             foreach (var note in _notes)
             {
                 double timeDiff = note.HitTime - _currentTime;
@@ -33,18 +53,73 @@ namespace Comp_296_project_ARMA.Objects
 
                 float _noteXPosition = PlayFieldConstants.StartX + note.Lane * PlayFieldConstants.LaneWidth;
 
+                Color _noteColor = note.Lane switch
+                {
+                    0 => Color.Red,
+                    1 => Color.CornflowerBlue,
+                    2 => Color.CornflowerBlue,
+                    3 => Color.Red,
+                    _ => Color.White
+                };
+
+                if (note.IsHold)
+                {
+                    // Draw unstarted hold notes normally
+                    DrawHoldNote(spriteBatch, note, _noteXPosition, _noteYPosition, _noteColor, _currentTime);
+                }
+
                 Rectangle noteRect = new Rectangle(
                     (int)_noteXPosition + 2,
                     (int)_noteYPosition,
                     PlayFieldConstants.LaneWidth - 4,
                     PlayFieldConstants.NoteHeight
-            );
-                spriteBatch.Draw(_noteTexture, noteRect, Color.White);
+                );
+                spriteBatch.Draw(_noteTexture, noteRect, _noteColor);
             }
+
 
         }
 
-       
+    
+
+        public void DrawHoldNote(SpriteBatch spriteBatch, NoteObject note,
+            float _noteXPosition, float _noteYPosition, Color noteColor, double currentTime)
+        {
+            double _tailTimeDiff = note.HoldEndTime - currentTime;
+            float _tailY = PlayFieldConstants.ReceptorY - (float)(_tailTimeDiff * _scrollSpeed);
+
+            _tailY = Math.Min(_tailY, PlayFieldConstants.ReceptorY);
+
+            // Draw body
+            Rectangle holdRect = new Rectangle(
+                    (int)_noteXPosition + 10,
+                    (int)_tailY,
+                    PlayFieldConstants.LaneWidth - 20,
+                    (int)(_noteYPosition - _tailY)
+            );
+            spriteBatch.Draw(_noteTexture, holdRect, noteColor
+             * 0.5f); // Semi-transparent body
+            
+            // Draw head
+            Rectangle headRect = new Rectangle(
+                    (int)_noteXPosition + 2,
+                    (int)_noteYPosition,
+                    PlayFieldConstants.LaneWidth - 4,
+                    PlayFieldConstants.NoteHeight
+            );
+            spriteBatch.Draw(_noteTexture, headRect, noteColor);
+
+            // Draw tail
+            Rectangle tailRect = new Rectangle(
+                    (int)_noteXPosition + 2,
+                    (int)_tailY,
+                    PlayFieldConstants.LaneWidth - 4,
+                    PlayFieldConstants.NoteHeight
+            );
+            spriteBatch.Draw(_noteTexture, tailRect, noteColor);
+        }
+
+
     }
 
 }
